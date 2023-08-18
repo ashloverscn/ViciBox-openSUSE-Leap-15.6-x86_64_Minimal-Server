e3e62ca46a083d38b4095a55aedca7a74996e3f2
ashloverscn <ashloverscn@gmail.com>
2023-08-18 21:55:13 +0530

    Update enable-PrintMotd.sh

--- enable-PrintMotd.sh
+++ enable-PrintMotd.sh
@@ -1,5 +1,30 @@
 #!/bin/sh
 
+mv /root/.bashrc /root/.bashrc.bak
+
+tee -a /root/.bashrc <<EOF
+
+# .bashrc
+
+# User specific aliases and functions
+
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+        . /etc/bashrc
+fi
+
+export PERL_LOCAL_LIB_ROOT="$PERL_LOCAL_LIB_ROOT:/root/perl5";
+export PERL_MB_OPT="--install_base /root/perl5";
+export PERL_MM_OPT="INSTALL_BASE=/root/perl5";
+export PERL5LIB="/root/perl5/lib/perl5:$PERL5LIB";
+export PATH="/root/perl5/bin:$PATH";
+
+EOF
+
 mv /etc/motd /etc/motd.bak
 \cp -r /usr/src/motd /etc/motd
 
@@ -12,6 +37,6 @@ sed -i "s|#PrintMotd yes|PrintMotd yes|g" /etc/ssh/sshd_config
 rm -rf ~/.hushlogin
 #touch ~/.hushlogin
 
-echo "ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p'" >> ~/.bashrc
+#echo "ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p'" >> ~/.bashrc
 
 
