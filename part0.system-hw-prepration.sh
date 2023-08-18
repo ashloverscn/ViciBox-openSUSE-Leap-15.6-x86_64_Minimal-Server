66fed138253ff4c8ed3777835fade137829fb518
ashloverscn <ashloverscn@gmail.com>
2023-08-18 14:55:32 +0200

    Add files via upload

---
+++ part0.system-hw-prepration.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+echo -e "\e[0;32m ViciBox-openSUSE-Leap-15.3.3-SCRATCH-Minimal-Server \e[0m"
+echo -e "\e[0;32m Part0 System-HW-Prepration \e[0m"
+sleep 5
+
+export LC_ALL=C
+
+# part 0
+#we will later blow holes in the firewall for now its off
+systemctl disable firewalld
+systemctl stop firewalld
+#disable ipv6 system-wide 
+cat /etc/sysctl.d/70-yast.conf
+echo "" > /etc/sysctl.d/70-yast.conf
+#if already present then dont add the lines 
+sed -i -e '$a\
+\
+net.ipv4.ip_forward = 0 \
+net.ipv6.conf.all.forwarding = 0 \
+net.ipv6.conf.all.disable_ipv6 = 1 \
+' /etc/sysctl.d/70-yast.conf
+
+systemctl restart network
+service network restart
+
+#enable verbose boot 
+sed -i 's/quiet//g' /etc/default/grub
+sed -i 's/splash=silent/splash=verbose/g' /etc/default/grub
+grub2-mkconfig -o /boot/grub2/grub.cfg
+
+#reboot
